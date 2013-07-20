4edf177b7a91399af1e1020d35a2ba444a01650e
VectorChief <vectorchiefrocks@gmail.com>
2013-07-20 15:37:56 +0400

    Linux makefiles updated

--- test/simd_make_arm.mk
+++ test/simd_make_arm.mk
@@ -5,5 +5,5 @@ SRC_LIST =                          \
         simd_test.cpp
 
 simd_test:
-	g++ -O3 -g -DRT_ARM             \
+	g++ -O3 -g -DRT_ARM -DRT_DEBUG=0 \
         ${INC_PATH} ${SRC_LIST} -o simd_test.arm
--- test/simd_make_x86.mk
+++ test/simd_make_x86.mk
@@ -5,5 +5,5 @@ SRC_LIST =                          \
         simd_test.cpp
 
 simd_test:
-	g++ -O3 -g -DRT_X86             \
+	g++ -O3 -g -DRT_X86 -DRT_DEBUG=0 \
         ${INC_PATH} ${SRC_LIST} -o simd_test.x86
