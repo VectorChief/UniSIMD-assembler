e03c4a69ec4cce8340d0d8c6e507e2b52841d452
VectorChief <vectorchiefrocks@gmail.com>
2014-06-29 19:31:03 +0300

    empty project, initial file set and directory structure

--- test/simd_make_arm.mk
+++
@@ -1,9 +0,0 @@
-INC_PATH =                          \
-        -I../core/
-
-SRC_LIST =                          \
-        simd_test.cpp
-
-simd_test:
-	g++ -O3 -g -DRT_ARM             \
-        ${INC_PATH} ${SRC_LIST} -o simd_test.arm
--- test/simd_make_x86.mk
+++
@@ -1,9 +0,0 @@
-INC_PATH =                          \
-        -I../core/
-
-SRC_LIST =                          \
-        simd_test.cpp
-
-simd_test:
-	g++ -O3 -g -DRT_X86             \
-        ${INC_PATH} ${SRC_LIST} -o simd_test.x86
