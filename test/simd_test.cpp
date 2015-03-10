d3254fb285325a626c1b9390feaae8d24fdeed98
VectorChief <vectorchiefrocks@gmail.com>
2015-03-10 14:42:04 +0200

    update copyright year to 2015

--- test/simd_test.cpp
+++ test/simd_test.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************/
-/* Copyright (c) 2013-2014 VectorChief (at github, bitbucket, sourceforge)    */
+/* Copyright (c) 2013-2015 VectorChief (at github, bitbucket, sourceforge)    */
 /* Distributed under the MIT software license, see the accompanying           */
 /* file COPYING or http://www.opensource.org/licenses/mit-license.php         */
 /******************************************************************************/
