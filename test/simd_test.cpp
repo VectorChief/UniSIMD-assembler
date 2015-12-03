387a5f48404decbb9dcdd79d11e3ce93698b1bc8
VectorChief <vectorchiefrocks@gmail.com>
2015-12-03 21:42:15 +0400

    replace non-standard malloc.h with stdlib.h for malloc/free

--- test/simd_test.cpp
+++ test/simd_test.cpp
@@ -4,7 +4,7 @@
 /* file COPYING or http://www.opensource.org/licenses/mit-license.php         */
 /******************************************************************************/
 
-#include <malloc.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
 
