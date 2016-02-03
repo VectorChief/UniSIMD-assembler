c7e265b075796c5a4862a74f1588de0ba1778774
VectorChief <vectorchiefrocks@gmail.com>
2016-02-03 22:01:40 +0200

    adjust makefiles for clang (3.6 tested)

--- test/simd_make_x86.mk
+++ test/simd_make_x86.mk
@@ -1,11 +1,16 @@
 
-INC_PATH =                          \
+INC_PATH =                              \
         -I../core/
 
-SRC_LIST =                          \
+SRC_LIST =                              \
         simd_test.cpp
+
+LIB_PATH =
+
+LIB_LIST =                              \
+        -lm
 
 simd_test:
 	g++ -O3 -g -m32 \
         -DRT_LINUX -DRT_X86 -DRT_DEBUG=1 \
-        ${INC_PATH} ${SRC_LIST} -o simd_test.x86
+        ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.x86
