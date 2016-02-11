3d23f51d5de6f29898da66ada438e920be164a80
VectorChief <vectorchiefrocks@gmail.com>
2016-02-11 15:47:29 +0000

    treat default ARM target as vanilla, add Nokia N900, Raspberry Pi 2

--- test/simd_make_arm.mk
+++ test/simd_make_arm.mk
@@ -14,3 +14,8 @@ simd_test:
 	g++ -O3 -g \
         -DRT_LINUX -DRT_ARM -DRT_128=1 -DRT_DEBUG=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.arm
+
+# 0) The build flags above are intended for the default "vanilla" ARMv7 target,
+# while settings suitable for a specific hardware platform are presented below.
+# 1) Nokia N900, Maemo 5 scratchbox: "vanilla"
+# 2) Raspberry Pi 2, Raspbian: -DRT_ARM=2
