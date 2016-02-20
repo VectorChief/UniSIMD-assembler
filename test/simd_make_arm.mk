b9fc04e442c324d63dccb58e1173026541f79af9
VectorChief <vectorchiefrocks@gmail.com>
2016-02-20 19:10:17 +0200

    use arm-linux-gnueabi cross-compiler name for ARM (works natively as well)

--- test/simd_make_arm.mk
+++ test/simd_make_arm.mk
@@ -11,11 +11,11 @@ LIB_LIST =                              \
         -lm
 
 simd_test:
-	g++ -O3 -g -static \
+	arm-linux-gnueabi-g++ -O3 -g -static \
         -DRT_LINUX -DRT_ARM -DRT_128=1 -DRT_DEBUG=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.arm
 
-# 0) The build flags above are intended for the default "vanilla" ARMv7 target,
-# while settings suitable for a specific hardware platform are presented below.
+# 0) Build flags above are intended for default "vanilla" ARMv7 target, while
+# settings suitable for specific hardware platform are given below (replace).
 # 1) Nokia N900, Maemo 5 scratchbox: "vanilla"
-# 2) Raspberry Pi 2, Raspbian: -DRT_ARM=2
+# 2) Raspberry Pi 2, Raspbian: arm-linux-gnueabihf-g++ -DRT_ARM=2
