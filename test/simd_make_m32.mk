6cd8931f81e6cf2fe01a1eafcaf2a6ed7f36110c
VectorChief <vectorchiefrocks@gmail.com>
2016-02-20 16:16:24 +0200

    use MSA-aware Codescape GNU Toolchain 2015.01-5 for MIPS

--- test/simd_make_m32.mk
+++ test/simd_make_m32.mk
@@ -11,6 +11,6 @@ LIB_LIST =                              \
         -lm
 
 simd_test:
-	mipsel-linux-gnu-g++ -O3 -g \
+	mips-mti-linux-gnu-g++ -O3 -g -EL -mips32r2 -mmsa \
         -DRT_LINUX -DRT_M32 -DRT_128=1 -DRT_DEBUG=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m32
