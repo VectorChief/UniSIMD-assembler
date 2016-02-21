281836d4dcb8c82ea071d9dd9002ca87fab0a869
VectorChief <vectorchiefrocks@gmail.com>
2016-02-22 01:17:16 +0200

    use standart triplet-name for g++ on x86_64 host

--- test/simd_make_x32.mk
+++ test/simd_make_x32.mk
@@ -11,7 +11,7 @@ LIB_LIST =                              \
         -lm
 
 simd_test:
-	g++ -O3 -g -static \
+	x86_64-linux-gnu-g++ -O3 -g -static \
         -DRT_LINUX -DRT_X32 -DRT_128=2 -DRT_DEBUG=1 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.x32
