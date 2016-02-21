4784d1b3f24999f94cc65d2b8f0adb2915969759
VectorChief <vectorchiefrocks@gmail.com>
2016-02-22 00:29:17 +0200

    test 64/32-bit ptrs/adrs hybrid support on a32/x32 targets

--- test/simd_make_a32.mk
+++ test/simd_make_a32.mk
@@ -11,7 +11,11 @@ LIB_LIST =                              \
         -lm
 
 simd_test:
-	aarch64-linux-gnu-g++ -O3 -g -static -mabi=ilp32 \
+	aarch64-linux-gnu-g++ -O3 -g -static \
         -DRT_LINUX -DRT_A32 -DRT_128=1 -DRT_DEBUG=1 \
-        -DRT_POINTER=32 -DRT_ADDRESS=32 -DRT_ENDIAN=0 \
+        -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.a32
+
+# For actual a32 target use: -mabi=ilp32, -DRT_POINTER=32 (replace).
+# Experimental 64/32-bit hybrid support is enabled for compatibility
+# with wider spectrum of toolchains/libraries in the standard a64 target.
--- test/simd_make_x32.mk
+++ test/simd_make_x32.mk
@@ -11,7 +11,11 @@ LIB_LIST =                              \
         -lm
 
 simd_test:
-	g++ -O3 -g -static -mx32 \
+	g++ -O3 -g -static \
         -DRT_LINUX -DRT_X32 -DRT_128=2 -DRT_DEBUG=1 \
-        -DRT_POINTER=32 -DRT_ADDRESS=32 -DRT_ENDIAN=0 \
+        -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.x32
+
+# For actual x32 target use: -mx32, -DRT_POINTER=32 (replace).
+# Experimental 64/32-bit hybrid support is enabled for compatibility
+# with wider spectrum of toolchains/libraries in the standard x64 target.
