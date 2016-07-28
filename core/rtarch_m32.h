2265bb5d52548e9a46e694c3fe454536ecfe5e0f
VectorChief <vectorchiefrocks@gmail.com>
2016-07-28 21:32:24 +0400

    fix non-setting-flags BASE ops on MIPS

--- core/rtarch_m32.h
+++ core/rtarch_m32.h
@@ -521,9 +521,9 @@
 
 #define notxx_mm(RM, DP)                                                    \
         AUW(SIB(RM),  EMPTY,  EMPTY,    MOD(RM), VAL(DP), C1(DP), EMPTY2)   \
-        EMITW(0x8C000000 | MDM(TMxx,    MOD(RM), VAL(DP), B1(DP), P1(DP)))  \
-        EMITW(0x00000027 | MRM(TMxx,    TZxx,    TMxx))                     \
-        EMITW(0xAC000000 | MDM(TMxx,    MOD(RM), VAL(DP), B1(DP), P1(DP)))
+        EMITW(0x8C000000 | MDM(TIxx,    MOD(RM), VAL(DP), B1(DP), P1(DP)))  \
+        EMITW(0x00000027 | MRM(TIxx,    TZxx,    TIxx))                     \
+        EMITW(0xAC000000 | MDM(TIxx,    MOD(RM), VAL(DP), B1(DP), P1(DP)))
 
 /* neg
  * set-flags: undefined (xx), yes (zx) */
