4811a92d8f03e1e0d77f007d4fc8fbc87fcb0867
VectorChief <vectorchiefrocks@gmail.com>
2016-08-08 21:20:48 +0400

    adjust debug condition in custom allocators

--- test/simd_test.cpp
+++ test/simd_test.cpp
@@ -2797,11 +2797,11 @@ rt_pntr sys_alloc(rt_size size)
 
 #endif /* RT_POINTER */
 
-#if RT_POINTER == 64 && RT_DEBUG >= 1
+#if RT_DEBUG >= 1
 
     RT_LOGI("ALLOC PTR = %016"RT_PR64"X, size = %ld\n", (rt_full)ptr, size);
 
-#endif /* RT_POINTER && RT_DEBUG */
+#endif /* RT_DEBUG */
 
 #if (RT_POINTER - RT_ADDRESS) != 0
 
@@ -2837,11 +2837,11 @@ rt_void sys_free(rt_pntr ptr, rt_size size)
 
 #endif /* RT_POINTER */
 
-#if RT_POINTER == 64 && RT_DEBUG >= 1
+#if RT_DEBUG >= 1
 
     RT_LOGI("FREED PTR = %016"RT_PR64"X, size = %ld\n", (rt_full)ptr, size);
 
-#endif /* RT_POINTER && RT_DEBUG */
+#endif /* RT_DEBUG */
 }
 
 #elif defined (RT_LINUX) /* Linux, GCC -------------------------------------- */
@@ -2898,11 +2898,11 @@ rt_pntr sys_alloc(rt_size size)
 
 #endif /* RT_POINTER */
 
-#if RT_POINTER == 64 && RT_DEBUG >= 1
+#if RT_DEBUG >= 1
 
     RT_LOGI("ALLOC PTR = %016"RT_PR64"X, size = %ld\n", (rt_full)ptr, size);
 
-#endif /* RT_POINTER && RT_DEBUG */
+#endif /* RT_DEBUG */
 
 #if (RT_POINTER - RT_ADDRESS) != 0
 
@@ -2938,11 +2938,11 @@ rt_void sys_free(rt_pntr ptr, rt_size size)
 
 #endif /* RT_POINTER */
 
-#if RT_POINTER == 64 && RT_DEBUG >= 1
+#if RT_DEBUG >= 1
 
     RT_LOGI("FREED PTR = %016"RT_PR64"X, size = %ld\n", (rt_full)ptr, size);
 
-#endif /* RT_POINTER && RT_DEBUG */
+#endif /* RT_DEBUG */
 }
 
 #endif /* ------------- OS specific ----------------------------------------- */
