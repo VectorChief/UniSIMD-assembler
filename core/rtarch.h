a7053bee0bece95a8d8f4e9015bdb02fb7f106f0
VectorChief <vectorchiefrocks@gmail.com>
2016-10-27 18:44:25 +0300

    update current/future target descriptions with new findings

--- core/rtarch.h
+++ core/rtarch.h
@@ -38,45 +38,67 @@
  *  - rtarch_arm.h         - AArch32:ARMv7 ISA, 16 BASE regs, 8 + temps used
  *  - rtarch_arm_128.h     - 32-bit elements, 16 SIMD regs, 8 + temps used
  *  - rtarch_x86.h         - x86 32-bit ISA, 8 BASE regs, 6 + esp, ebp used
- *  - rtarch_x86_128.h     - 32-bit elements, 8 SIMD regs, 8 used, SSE 128-bit
- *  - rtarch_x86_256.h     - 32-bit elements, 8 SIMD regs, 8 used, AVX 256-bit
+ *  - rtarch_x86_128.h     - 32-bit elements, 8 SIMD regs, SSE 128-bit, 8 used
+ *  - rtarch_x86_256.h     - 32-bit elements, 8 SIMD regs, AVX 256-bit, 8 used
  *
  * Current 32/64-bit BASE
  * and 32/64-bit SIMD targets:
  *
  *  - rtarch_a32.h         - AArch64:ILP32 ABI, 32 BASE regs, 14 + temps used
  *  - rtarch_a64.h         - AArch64:ARMv8 ISA, 32 BASE regs, 14 + temps used
- *  - rtarch_a32_128.h     - 32-bit elements, 32 SIMD regs, NEON 128-bit
- *  - rtarch_a64_128.h     - 64-bit elements, 32 SIMD regs, NEON 128-bit
+ *  - rtarch_a32_128.h     - 32-bit elements, 32 SIMD regs, NEON 128-bit, 16+1
+ *  - rtarch_a64_128.h     - 64-bit elements, 32 SIMD regs, NEON 128-bit, 16+1
  *  - rtarch_m32.h         - MIPS32 r5/r6 ISA, 32 BASE regs, 14 + temps used
  *  - rtarch_m64.h         - MIPS64 r5/r6 ISA, 32 BASE regs, 14 + temps used
- *  - rtarch_m32_128.h     - 32-bit elements, 32 SIMD regs, MSA 128-bit
- *  - rtarch_m64_128.h     - 64-bit elements, 32 SIMD regs, MSA 128-bit
+ *  - rtarch_m32_128.h     - 32-bit elements, 32 SIMD regs, MSA 128-bit, 16+4(2)
+ *  - rtarch_m64_128.h     - 64-bit elements, 32 SIMD regs, MSA 128-bit, 16+4(2)
  *  - rtarch_p32.h         - Power 32-bit ISA, 32 BASE regs, 14 + temps used
  *  - rtarch_p64.h         - Power 64-bit ISA, 32 BASE regs, 14 + temps used
- *  - rtarch_p32_128.h     - 32-bit elements, 32 SIMD regs, VMX/VSX 128-bit
- *  - rtarch_p64_128.h     - 64-bit elements, 32 SIMD regs, VMX/VSX 128-bit
+ *  - rtarch_p32_128.h     - 32-bit elements, 32 SIMD regs, VMX/VSX 128-bit, 16+
+ *  - rtarch_p64_128.h     - 64-bit elements, 32 SIMD regs, VMX/VSX 128-bit, 16+
  *  - rtarch_x32.h         - x86_64:x32 ABI, 16 BASE regs, 14 + temps used
  *  - rtarch_x64.h         - x86_64:x64 ISA, 16 BASE regs, 14 + temps used
- *  - rtarch_x32_128.h     - 32-bit elements, 16 SIMD regs, SSE 128-bit
- *  - rtarch_x64_128.h     - 64-bit elements, 16 SIMD regs, SSE 128-bit
- *  - rtarch_x32_256.h     - 32-bit elements, 16 SIMD regs, AVX 256-bit
- *  - rtarch_x64_256.h     - 64-bit elements, 16 SIMD regs, AVX 256-bit
+ *  - rtarch_x32_128.h     - 32-bit elements, 16 SIMD regs, SSE 128-bit, 16 used
+ *  - rtarch_x64_128.h     - 64-bit elements, 16 SIMD regs, SSE 128-bit, 16 used
+ *  - rtarch_x32_256.h     - 32-bit elements, 16 SIMD regs, AVX 256-bit, 16 used
+ *  - rtarch_x64_256.h     - 64-bit elements, 16 SIMD regs, AVX 256-bit, 16 used
  *
  * Future 32-bit SIMD targets:
  *
- *  - rtarch_a32_256.h     - 32-bit elements, 32 SIMD regs, SVE 256-bit
- *  - rtarch_a32_512.h     - 32-bit elements, 32 SIMD regs, SVE 512-bit
- *  - rtarch_x32_512.h     - 32-bit elements, 32 SIMD regs, AVX 512-bit
- *  - rtarch_x86_512.h     - 32-bit elements,  8 SIMD regs, AVX 512-bit
+ *  - rtarch_a32_256.h     - 32-bit elements, 16 SIMD regs, pairs of 128-bit, 15
+ *  - rtarch_a32_256.h     - 32-bit elements, 32 SIMD regs, SVE 256-bit, 30 used
+ *  - rtarch_a32_512.h     - 32-bit elements, 32 SIMD regs, SVE 512-bit, 30 used
+ *  - rtarch_m32_256.h     - 32-bit elements, 16 SIMD regs, pairs of 128-bit, 15
+ *  - rtarch_p32_256.h     - 32-bit elements, 16 SIMD regs, pairs of 128-bit, 15
+ *  - rtarch_p32_256.h     - 32-bit elements, 32 SIMD regs, pairs of 128-bit, 30
+ *  - rtarch_p32_512.h     - 32-bit elements, 16 SIMD regs, quads of 128-bit, 15
+ *  - rtarch_x32_512.h     - 32-bit elements, 16 SIMD regs, AVX 512-bit, 16 used
+ *  - rtarch_x32_512.h     - 32-bit elements, 32 SIMD regs, AVX 512-bit, 30 used
+ *  - rtarch_x86_512.h     - 32-bit elements, 8 SIMD regs, AVX 512-bit, 8 used
  *
  * Future 64-bit SIMD targets:
  *
- *  - rtarch_a64_256.h     - 64-bit elements, 32 SIMD regs, SVE 256-bit
- *  - rtarch_a64_512.h     - 64-bit elements, 32 SIMD regs, SVE 512-bit
- *  - rtarch_x64_512.h     - 64-bit elements, 32 SIMD regs, AVX 512-bit
+ *  - rtarch_a64_256.h     - 64-bit elements, 16 SIMD regs, pairs of 128-bit, 15
+ *  - rtarch_a64_256.h     - 64-bit elements, 32 SIMD regs, SVE 256-bit, 30 used
+ *  - rtarch_a64_512.h     - 64-bit elements, 32 SIMD regs, SVE 512-bit, 30 used
+ *  - rtarch_m64_256.h     - 64-bit elements, 16 SIMD regs, pairs of 128-bit, 15
+ *  - rtarch_p64_256.h     - 64-bit elements, 16 SIMD regs, pairs of 128-bit, 15
+ *  - rtarch_p64_256.h     - 64-bit elements, 32 SIMD regs, pairs of 128-bit, 30
+ *  - rtarch_p64_512.h     - 64-bit elements, 16 SIMD regs, quads of 128-bit, 15
+ *  - rtarch_x64_512.h     - 64-bit elements, 16 SIMD regs, AVX 512-bit, 16 used
+ *  - rtarch_x64_512.h     - 64-bit elements, 32 SIMD regs, AVX 512-bit, 30 used
  *
- * Preliminary naming scheme for extended BASE and SIMD register files.
+ * not all registers in target descriptions are always exposed for apps to use
+ * flags RT_BASE_REGS and RT_SIMD_REGS are available for rough differentiation
+ * between register-file sizes with current values: legacy 8, 16, 32 (planned)
+ *
+ * fixed 256-bit ops can be done as pairs with 2*15 128-bit regs on modern RISCs
+ * fixed 256-bit ops can be done as pairs with 2*30 128-bit regs on modern Power
+ * while 512-bit ops can be done as quads with 4*15 128-bit regs on modern Power
+ * potentially saving extra address-calculation step using indexed-load on Power
+ * in case of only 14/28 exposed SIMD registers some rare ops can be done faster
+ *
+ * Preliminary naming scheme for extended BASE and SIMD register-files.
  *
  * Legacy 8 BASE and 8 SIMD registers:
  *  - Reax, Rebx, Recx, Redx, Resp, Rebp, Resi, Redi
@@ -138,9 +160,10 @@
  * cmdf*_** - SIMD-data args, SIMD ISA (32/64-bit configurable, packed-256-bit)
  *
  * fixed 256-bit ops can be done as pairs with 2*15 128-bit regs on modern RISCs
+ * fixed 256-bit ops can be done as pairs with 2*30 128-bit regs on modern Power
  * while 512-bit ops can be done as quads with 4*15 128-bit regs on modern Power
- * potentially saving extra address-calculation step using indexed load on Power
- * in case of only 14 exposed SIMD registers some rare ops can be done faster
+ * potentially saving extra address-calculation step using indexed-load on Power
+ * in case of only 14/28 exposed SIMD registers some rare ops can be done faster
  *
  * The following BASE instruction namespaces are planned for potential future.
  *
