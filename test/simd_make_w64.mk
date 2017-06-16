2feef4376eb24be22b9d3c0e11817e3b79219135
VectorChief <vectorchiefrocks@gmail.com>
2017-06-16 22:49:38 +0300

    clean up Win64 makefiles for consistency with x64 on Linux

--- test/simd_make_w64.mk
+++ test/simd_make_w64.mk
@@ -22,25 +22,25 @@ clean:
 
 simd_test_w64_32:
 	g++ -O3 -g -static -m64 \
-        -DRT_WIN64 -DRT_X64 -DRT_256_R8=2 -DRT_DEBUG=0 \
+        -DRT_WIN64 -DRT_X64 -DRT_256_R8=4 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test_w64_32.exe
 
 simd_test_w64_64:
 	g++ -O3 -g -static -m64 \
-        -DRT_WIN64 -DRT_X64 -DRT_256_R8=2 -DRT_DEBUG=0 \
+        -DRT_WIN64 -DRT_X64 -DRT_256_R8=4 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=64 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test_w64_64.exe
 
 simd_test_w64f32:
 	g++ -O3 -g -static -m64 \
-        -DRT_WIN64 -DRT_X64 -DRT_256_R8=2 -DRT_DEBUG=0 \
+        -DRT_WIN64 -DRT_X64 -DRT_256_R8=4 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test_w64f32.exe
 
 simd_test_w64f64:
 	g++ -O3 -g -static -m64 \
-        -DRT_WIN64 -DRT_X64 -DRT_256_R8=2 -DRT_DEBUG=0 \
+        -DRT_WIN64 -DRT_X64 -DRT_256_R8=4 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=64 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test_w64f64.exe
 
@@ -57,19 +57,19 @@ simd_test_w64f64:
 
 # For interpretation of SIMD build flags check compatibility layer in rtzero.h
 
-# For 128-bit AVX3.2 build use (replace): RT_128=1 (Skylake X and AVX-512++)
-# For 128-bit SSE2 build use (replace): RT_128=2
+# For 128-bit 30-reg build use (replace): RT_128=1  (reserved for AVX1/2)
+# For 128-bit AVX3.2 build use (replace): RT_128=2 (Skylake X and AVX-512++)
+# For 128-bit SSE2 build use (replace): RT_128=4 RT_SIMD_COMPAT_SSE=2
 # For 128-bit SSE4 build use (replace): RT_128=4
-# For 128-bit AVX1 build use (replace): RT_128=8
-# For 128-bit AVX2 build use (replace): RT_128=8 RT_SIMD_COMPAT_128=2 (FMA3)
-# For 128-bit FMA3 build use (replace): RT_128=8 RT_SIMD_COMPAT_128=3 (FMA3)
-# For 128-bit FMA3 build use (replace): RT_128=16       (as above AVX1+FMA3)
+# For 128-bit AVX1 build use (replace): RT_128=8           (AMD's AVX1-only)
+# For 128-bit FMA3 build use (replace): RT_128=16          (AMD's AVX1+FMA3)
+# For 128-bit AVX2 build use (replace): RT_128=32          (AMD's AVX2+FMA3)
 
-# For 256-bit SSE2 build use (replace): RT_256_R8=2       (8 SIMD-reg pairs)
+# For 256-bit SSE2 build use (replace): RT_256_R8=4 RT_SIMD_COMPAT_SSE=2
 # For 256-bit SSE4 build use (replace): RT_256_R8=4       (8 SIMD-reg pairs)
-# For 256-bit AVX1 build use (replace): RT_256=1
-# For 256-bit AVX2 build use (replace): RT_256=2                      (FMA3)
-# For 256-bit FMA3 build use (replace): RT_256=4                 (AVX1+FMA3)
+# For 256-bit AVX1 build use (replace): RT_256=1         (Intel's AVX1-only)
+# For 256-bit AVX2 build use (replace): RT_256=2         (Intel's AVX2+FMA3)
+# For 256-bit 30-reg build use (replace): RT_256=4  (reserved for AVX1/2)
 # For 256-bit AVX3.2 build use (replace): RT_256=8 (Skylake X and AVX-512++)
 
 # For 512-bit AVX1 build use (replace): RT_512_R8=1       (8 SIMD-reg pairs)
