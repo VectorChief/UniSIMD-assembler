2d30c618a549547cc481d4709bf7dfc94eb7a2a1
VectorChief <vectorchiefrocks@gmail.com>
2018-10-27 22:44:43 +0300

    clean up deprecated x86 from macOS build scripts

--- test/build_macOS.sh
+++ test/build_macOS.sh
@@ -2,14 +2,10 @@
 # Intended for x86_64 Mac OS X / OS X / macOS build environment
 # with Command Line Tools installed (Mac OS X Lion / macOS High Sierra tested)
 
-make -f simd_make_x86.mk build -j16
 make -f simd_make_x64.mk build -j16
 
-make -f simd_make_x86.mk macRD
 make -f simd_make_x64.mk macRD
 
-make -f simd_make_x86.mk strip
 make -f simd_make_x64.mk strip
 
-make -f simd_make_x86.mk macOS
 make -f simd_make_x64.mk macOS
--- test/clean_macOS.sh
+++ test/clean_macOS.sh
@@ -2,5 +2,4 @@
 # Intended for x86_64 Mac OS X / OS X / macOS build environment
 # with Command Line Tools installed (Mac OS X Lion / macOS High Sierra tested)
 
-make -f simd_make_x86.mk macRM
 make -f simd_make_x64.mk macRM
