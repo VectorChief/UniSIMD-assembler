5ff4ac46a732ba437bd865e37fabfa924f40ff86
VectorChief <vectorchiefrocks@gmail.com>
2019-04-10 13:08:50 +0300

    adjust build instructions for older HW compatibility

--- test/build_multi.sh
+++ test/build_multi.sh
@@ -2,8 +2,8 @@
 # Intended for x86_64 Linux build environment
 # with native g++ multilib-compiler installed (64-bit Linux Mint 18 tested)
 
-make -f simd_make_x86.mk build -j16
-make -f simd_make_x32.mk build -j16
+make -f simd_make_x86.mk build -j3
+make -f simd_make_x32.mk build
 
 make -f simd_make_x86.mk strip
 make -f simd_make_x32.mk strip
