dc86a8c565c1a682fe694c0727dba24c51f3c341
VectorChief <vectorchiefrocks@gmail.com>
2019-07-03 17:59:36 +0300

    drop extended POWER targets from SIMD testing

--- test/simd_make_p64.mk
+++ test/simd_make_p64.mk
@@ -11,7 +11,7 @@ LIB_LIST =                              \
         -lm
 
 
-build: build_p9 build_pX build_le build_be
+build: build_p9 build_le build_be
 
 strip:
 	powerpc64le-linux-gnu-strip simd_test.p64???L*
@@ -110,25 +110,25 @@ build_be: simd_test_p64_32Bp7 simd_test_p64_64Bp7 \
 
 simd_test_p64_32Bp7:
 	powerpc64-linux-gnu-g++ -O2 -g -static \
-        -DRT_LINUX -DRT_P64 -DRT_256=4 -DRT_DEBUG=0 \
+        -DRT_LINUX -DRT_P64 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.p64_32Bp7
 
 simd_test_p64_64Bp7:
 	powerpc64-linux-gnu-g++ -O2 -g -static \
-        -DRT_LINUX -DRT_P64 -DRT_256=4 -DRT_DEBUG=0 \
+        -DRT_LINUX -DRT_P64 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=64 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.p64_64Bp7
 
 simd_test_p64f32Bp7:
 	powerpc64-linux-gnu-g++ -O2 -g -static \
-        -DRT_LINUX -DRT_P64 -DRT_512=1 -DRT_DEBUG=0 \
+        -DRT_LINUX -DRT_P64 -DRT_256=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=32 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.p64f32Bp7
 
 simd_test_p64f64Bp7:
 	powerpc64-linux-gnu-g++ -O2 -g -static \
-        -DRT_LINUX -DRT_P64 -DRT_512=1 -DRT_DEBUG=0 \
+        -DRT_LINUX -DRT_P64 -DRT_256=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=64 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.p64f64Bp7
 
