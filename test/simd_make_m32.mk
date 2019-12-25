39ec5c08c957752030a8eb5e1f1c68ba45a3f10e
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 16:44:25 +0400

    update build options for cross-compilation

--- test/simd_make_m32.mk
+++ test/simd_make_m32.mk
@@ -11,7 +11,7 @@ LIB_LIST =                              \
         -lm
 
 simd_test:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips32r5 -mmsa \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips32r5 -mmsa -mnan=2008 \
         -DRT_LINUX -DRT_M32 -DRT_128=1 -DRT_DEBUG=1 \
         -DRT_POINTER=32 -DRT_ADDRESS=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m32
