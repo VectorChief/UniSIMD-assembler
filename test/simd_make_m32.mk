424ba03941f59353721a40bbde6317ed2adbcc21
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 17:30:02 +0400

    update build options for cross-compilation

--- test/simd_make_m32.mk
+++ test/simd_make_m32.mk
@@ -22,25 +22,25 @@ clean:
 
 
 simd_test_m32Lr5:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips32r5 -mmsa \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips32r5 -mmsa -mnan=2008 \
         -DRT_LINUX -DRT_M32 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=32 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m32Lr5
 
 simd_test_m32Br5:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips32r5 -mmsa \
+	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips32r5 -mmsa -mnan=2008 \
         -DRT_LINUX -DRT_M32 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=32 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m32Br5
 
 simd_test_m32Lr6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EL -mips32r6 -mmsa \
+	mips-img-linux-gnu-g++ -O3 -g -static -EL -mips32r6 -mmsa -mnan=2008 \
         -DRT_LINUX -DRT_M32=6 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=32 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m32Lr6
 
 simd_test_m32Br6:
-	mips-img-linux-gnu-g++ -O3 -g -static -EB -mips32r6 -mmsa \
+	mips-img-linux-gnu-g++ -O3 -g -static -EB -mips32r6 -mmsa -mnan=2008 \
         -DRT_LINUX -DRT_M32=6 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=32 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m32Br6
