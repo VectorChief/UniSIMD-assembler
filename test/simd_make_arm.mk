3d18a59b7350514accce0ba75053f2b00b420fd3
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 16:52:44 +0400

    update build options on Linux (ARMv7)

--- test/simd_make_arm.mk
+++ test/simd_make_arm.mk
@@ -11,7 +11,7 @@ LIB_LIST =                              \
         -lm
 
 simd_test:
-	arm-linux-gnueabi-g++ -O3 -g -static \
+	arm-linux-gnueabi-g++ -O3 -g -static -march=armv7-a -marm \
         -DRT_LINUX -DRT_ARM -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=32 -DRT_ADDRESS=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.arm
