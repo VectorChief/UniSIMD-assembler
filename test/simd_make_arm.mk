3e461da856cf2bc4f3fe46bd12c63db2ac9e0413
VectorChief <vectorchiefrocks@gmail.com>
2019-12-25 16:13:42 +0400

    update build options on Linux (add -static for QEMU)

--- test/simd_make_arm.mk
+++ test/simd_make_arm.mk
@@ -11,6 +11,6 @@ LIB_LIST =                              \
         -lm
 
 simd_test:
-	g++ -O3 -g \
+	g++ -O3 -g -static -march=armv7-a -marm \
         -DRT_LINUX -DRT_ARM -DRT_128=1 -DRT_DEBUG=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.arm
