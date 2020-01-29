8a5f8dd6ac07f4e7390553d32702f1c0a32be16f
VectorChief <vectorchiefrocks@gmail.com>
2020-01-29 23:13:22 +0200

    add pragma for VC++ warning about frame pointer

--- core/config/rtbase.h
+++ core/config/rtbase.h
@@ -376,6 +376,7 @@ typedef unsigned int        rt_ui32;
 #pragma warning (disable : 4244) /* VC++ 6.0: conversion from double to float */
 #pragma warning (disable : 4305) /* VC++ 6.0: truncation from double to float */
 #pragma warning (disable : 4291) /* VC++ 6.0: operator new no matching delete */
+#pragma warning (disable : 4731) /* VC++ 9.0: frame ptr modified by assembler */
 #pragma warning (disable : 4838) /* VS 2017:  conversion from double to float */
 #pragma warning (disable : 4996) /* VS 2017:  secure form of string functions */
 
