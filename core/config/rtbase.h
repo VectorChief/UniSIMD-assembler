0c777d47dfad05dc519d748d413d1b1b44715f2c
VectorChief <vectorchiefrocks@gmail.com>
2020-02-19 13:00:22 +0200

    fix RISC targets with clang after version 6.0

--- core/config/rtbase.h
+++ core/config/rtbase.h
@@ -377,7 +377,7 @@ typedef unsigned int        rt_ui32;
 #pragma warning (disable : 4305) /* VC++ 6.0: truncation from double to float */
 #pragma warning (disable : 4291) /* VC++ 6.0: operator new no matching delete */
 #pragma warning (disable : 4731) /* VC++ 9.0: frame ptr modified by assembler */
-#pragma warning (disable : 4838) /* VC++ 9.0: conversion from double to float */
+#pragma warning (disable : 4838) /* VC++ X.0: conversion from double to float */
 #pragma warning (disable : 4996) /* VC++ 9.0: secure form of string functions */
 
 typedef __int64             rt_si64;
