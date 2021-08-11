9d33af252ebee3d79c24e7f52aea7a40773a7b82
VectorChief <vectorchiefrocks@gmail.com>
2021-08-11 18:52:55 +0300

    update build and test scripts

--- test/build_linux.sh
+++ test/build_linux.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # Intended for x86_64 Linux build environment
 # with native g++ compiler installed (64-bit Linux Mint 18 tested)
+# works on Ubuntu MATE 18.04/20.04 LTS (binaries aren't backward compatible)
 
 make -f simd_make_x64.mk build -j8
 
--- test/simd_test86.sh
+++ test/simd_test86.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # Intended for x86_64 Linux test environment
 # with multilib capabilities (64-bit Linux Mint 18 tested)
+# run this script after bulid_multi.sh with multilib-compiler installed
 
 touch test86; rm test86
 
-# fully successful test pass results in test86 file of 30356 bytes (51 tests)
-# test pass on AVX2-only CPU results in test86 file of 23474 bytes (51 tests)
+# fully successful test pass results in test86 file of  30356 bytes (51 tests)
+# test pass on AVX2-only CPU results in test86 file of  23474 bytes (51 tests)
 # for any other CPU check the output or use Intel SDE within script
 
 
