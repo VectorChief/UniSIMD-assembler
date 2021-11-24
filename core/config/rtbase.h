dab3afeca8b8b9b7f5867ac0b953c403aaa428db
VectorChief <vectorchiefrocks@gmail.com>
2021-11-24 18:34:44 +0200

    add macros to initialize byte/half SIMD fields

--- core/config/rtbase.h
+++ core/config/rtbase.h
@@ -203,6 +203,57 @@
                             s[40]=s[41]=s[42]=s[43]=s[44]=s[45]=s[46]=s[47]=\
                             s[48]=s[49]=s[50]=s[51]=s[52]=s[53]=s[54]=s[55]=\
                             s[56]=s[57]=s[58]=s[59]=s[60]=s[61]=s[62]=s[63]=v
+#define RT_SIMD_WIDTH16     128
+#define RT_SIMD_SET16(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
+                            s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=\
+                            s[16]=s[17]=s[18]=s[19]=s[20]=s[21]=s[22]=s[23]=\
+                            s[24]=s[25]=s[26]=s[27]=s[28]=s[29]=s[30]=s[31]=\
+                            s[32]=s[33]=s[34]=s[35]=s[36]=s[37]=s[38]=s[39]=\
+                            s[40]=s[41]=s[42]=s[43]=s[44]=s[45]=s[46]=s[47]=\
+                            s[48]=s[49]=s[50]=s[51]=s[52]=s[53]=s[54]=s[55]=\
+                            s[56]=s[57]=s[58]=s[59]=s[60]=s[61]=s[62]=s[63]=\
+                            s[64]=s[65]=s[66]=s[67]=s[68]=s[69]=s[70]=s[71]=\
+                            s[72]=s[73]=s[74]=s[75]=s[76]=s[77]=s[78]=s[79]=\
+                            s[80]=s[81]=s[82]=s[83]=s[84]=s[85]=s[86]=s[87]=\
+                            s[88]=s[89]=s[90]=s[91]=s[92]=s[93]=s[94]=s[95]=\
+                        s[96]=s[97]=s[98]=s[99]=s[100]=s[101]=s[102]=s[103]=\
+                    s[104]=s[105]=s[106]=s[107]=s[108]=s[109]=s[110]=s[111]=\
+                    s[112]=s[113]=s[114]=s[115]=s[116]=s[117]=s[118]=s[119]=\
+                    s[120]=s[121]=s[122]=s[123]=s[124]=s[125]=s[126]=s[127]=v
+#define RT_SIMD_WIDTH08     256
+#define RT_SIMD_SET08(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
+                            s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=\
+                            s[16]=s[17]=s[18]=s[19]=s[20]=s[21]=s[22]=s[23]=\
+                            s[24]=s[25]=s[26]=s[27]=s[28]=s[29]=s[30]=s[31]=\
+                            s[32]=s[33]=s[34]=s[35]=s[36]=s[37]=s[38]=s[39]=\
+                            s[40]=s[41]=s[42]=s[43]=s[44]=s[45]=s[46]=s[47]=\
+                            s[48]=s[49]=s[50]=s[51]=s[52]=s[53]=s[54]=s[55]=\
+                            s[56]=s[57]=s[58]=s[59]=s[60]=s[61]=s[62]=s[63]=\
+                            s[64]=s[65]=s[66]=s[67]=s[68]=s[69]=s[70]=s[71]=\
+                            s[72]=s[73]=s[74]=s[75]=s[76]=s[77]=s[78]=s[79]=\
+                            s[80]=s[81]=s[82]=s[83]=s[84]=s[85]=s[86]=s[87]=\
+                            s[88]=s[89]=s[90]=s[91]=s[92]=s[93]=s[94]=s[95]=\
+                        s[96]=s[97]=s[98]=s[99]=s[100]=s[101]=s[102]=s[103]=\
+                    s[104]=s[105]=s[106]=s[107]=s[108]=s[109]=s[110]=s[111]=\
+                    s[112]=s[113]=s[114]=s[115]=s[116]=s[117]=s[118]=s[119]=\
+                    s[120]=s[121]=s[122]=s[123]=s[124]=s[125]=s[126]=s[127]=\
+                    s[128]=s[129]=s[130]=s[131]=s[132]=s[133]=s[134]=s[135]=\
+                    s[136]=s[137]=s[138]=s[139]=s[140]=s[141]=s[142]=s[143]=\
+                    s[144]=s[145]=s[146]=s[147]=s[148]=s[149]=s[150]=s[151]=\
+                    s[152]=s[153]=s[154]=s[155]=s[156]=s[157]=s[158]=s[159]=\
+                    s[160]=s[161]=s[162]=s[163]=s[164]=s[165]=s[166]=s[167]=\
+                    s[168]=s[169]=s[170]=s[171]=s[172]=s[173]=s[174]=s[175]=\
+                    s[176]=s[177]=s[178]=s[179]=s[180]=s[181]=s[182]=s[183]=\
+                    s[184]=s[185]=s[186]=s[187]=s[188]=s[189]=s[190]=s[191]=\
+                    s[192]=s[193]=s[194]=s[195]=s[196]=s[197]=s[198]=s[199]=\
+                    s[200]=s[201]=s[202]=s[203]=s[204]=s[205]=s[206]=s[207]=\
+                    s[208]=s[209]=s[210]=s[211]=s[212]=s[213]=s[214]=s[215]=\
+                    s[216]=s[217]=s[218]=s[219]=s[220]=s[221]=s[222]=s[223]=\
+                    s[224]=s[225]=s[226]=s[227]=s[228]=s[229]=s[230]=s[231]=\
+                    s[232]=s[233]=s[234]=s[235]=s[236]=s[237]=s[238]=s[239]=\
+                    s[240]=s[241]=s[242]=s[243]=s[244]=s[245]=s[246]=s[247]=\
+                    s[248]=s[249]=s[250]=s[251]=s[252]=s[253]=s[254]=s[255]=v
+
 #elif (RT_SIMD == 1024)
 
 #define RT_SIMD_ALIGN       128
@@ -214,6 +265,33 @@
                             s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=\
                             s[16]=s[17]=s[18]=s[19]=s[20]=s[21]=s[22]=s[23]=\
                             s[24]=s[25]=s[26]=s[27]=s[28]=s[29]=s[30]=s[31]=v
+#define RT_SIMD_WIDTH16     64
+#define RT_SIMD_SET16(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
+                            s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=\
+                            s[16]=s[17]=s[18]=s[19]=s[20]=s[21]=s[22]=s[23]=\
+                            s[24]=s[25]=s[26]=s[27]=s[28]=s[29]=s[30]=s[31]=\
+                            s[32]=s[33]=s[34]=s[35]=s[36]=s[37]=s[38]=s[39]=\
+                            s[40]=s[41]=s[42]=s[43]=s[44]=s[45]=s[46]=s[47]=\
+                            s[48]=s[49]=s[50]=s[51]=s[52]=s[53]=s[54]=s[55]=\
+                            s[56]=s[57]=s[58]=s[59]=s[60]=s[61]=s[62]=s[63]=v
+#define RT_SIMD_WIDTH08     128
+#define RT_SIMD_SET08(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
+                            s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=\
+                            s[16]=s[17]=s[18]=s[19]=s[20]=s[21]=s[22]=s[23]=\
+                            s[24]=s[25]=s[26]=s[27]=s[28]=s[29]=s[30]=s[31]=\
+                            s[32]=s[33]=s[34]=s[35]=s[36]=s[37]=s[38]=s[39]=\
+                            s[40]=s[41]=s[42]=s[43]=s[44]=s[45]=s[46]=s[47]=\
+                            s[48]=s[49]=s[50]=s[51]=s[52]=s[53]=s[54]=s[55]=\
+                            s[56]=s[57]=s[58]=s[59]=s[60]=s[61]=s[62]=s[63]=\
+                            s[64]=s[65]=s[66]=s[67]=s[68]=s[69]=s[70]=s[71]=\
+                            s[72]=s[73]=s[74]=s[75]=s[76]=s[77]=s[78]=s[79]=\
+                            s[80]=s[81]=s[82]=s[83]=s[84]=s[85]=s[86]=s[87]=\
+                            s[88]=s[89]=s[90]=s[91]=s[92]=s[93]=s[94]=s[95]=\
+                        s[96]=s[97]=s[98]=s[99]=s[100]=s[101]=s[102]=s[103]=\
+                    s[104]=s[105]=s[106]=s[107]=s[108]=s[109]=s[110]=s[111]=\
+                    s[112]=s[113]=s[114]=s[115]=s[116]=s[117]=s[118]=s[119]=\
+                    s[120]=s[121]=s[122]=s[123]=s[124]=s[125]=s[126]=s[127]=v
+
 #elif (RT_SIMD == 512)
 
 #define RT_SIMD_ALIGN       64
@@ -222,6 +300,20 @@
 #define RT_SIMD_WIDTH32     16
 #define RT_SIMD_SET32(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
                             s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=v
+#define RT_SIMD_WIDTH16     32
+#define RT_SIMD_SET16(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
+                            s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=\
+                            s[16]=s[17]=s[18]=s[19]=s[20]=s[21]=s[22]=s[23]=\
+                            s[24]=s[25]=s[26]=s[27]=s[28]=s[29]=s[30]=s[31]=v
+#define RT_SIMD_WIDTH08     64
+#define RT_SIMD_SET08(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
+                            s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=\
+                            s[16]=s[17]=s[18]=s[19]=s[20]=s[21]=s[22]=s[23]=\
+                            s[24]=s[25]=s[26]=s[27]=s[28]=s[29]=s[30]=s[31]=\
+                            s[32]=s[33]=s[34]=s[35]=s[36]=s[37]=s[38]=s[39]=\
+                            s[40]=s[41]=s[42]=s[43]=s[44]=s[45]=s[46]=s[47]=\
+                            s[48]=s[49]=s[50]=s[51]=s[52]=s[53]=s[54]=s[55]=\
+                            s[56]=s[57]=s[58]=s[59]=s[60]=s[61]=s[62]=s[63]=v
 
 #elif (RT_SIMD == 256) && !(defined RT_SVEX1)
 
@@ -231,6 +323,10 @@
 #define RT_SIMD_SET64(s, v)     RT_SIMD_SET64_256(s, v)
 #define RT_SIMD_WIDTH32         RT_SIMD_WIDTH32_256
 #define RT_SIMD_SET32(s, v)     RT_SIMD_SET32_256(s, v)
+#define RT_SIMD_WIDTH16         RT_SIMD_WIDTH16_256
+#define RT_SIMD_SET16(s, v)     RT_SIMD_SET16_256(s, v)
+#define RT_SIMD_WIDTH08         RT_SIMD_WIDTH08_256
+#define RT_SIMD_SET08(s, v)     RT_SIMD_SET08_256(s, v)
 
 #elif (RT_SIMD == 256)
 
@@ -239,6 +335,10 @@
 #define RT_SIMD_SET64(s, v)     RT_SIMD_SET64_256(s, v)
 #define RT_SIMD_WIDTH32         RT_SIMD_WIDTH32_256
 #define RT_SIMD_SET32(s, v)     RT_SIMD_SET32_256(s, v)
+#define RT_SIMD_WIDTH16         RT_SIMD_WIDTH16_256
+#define RT_SIMD_SET16(s, v)     RT_SIMD_SET16_256(s, v)
+#define RT_SIMD_WIDTH08         RT_SIMD_WIDTH08_256
+#define RT_SIMD_SET08(s, v)     RT_SIMD_SET08_256(s, v)
 
 #elif (RT_SIMD == 128)
 
@@ -248,6 +348,10 @@
 #define RT_SIMD_SET64(s, v)     RT_SIMD_SET64_128(s, v)
 #define RT_SIMD_WIDTH32         RT_SIMD_WIDTH32_128
 #define RT_SIMD_SET32(s, v)     RT_SIMD_SET32_128(s, v)
+#define RT_SIMD_WIDTH16         RT_SIMD_WIDTH16_128
+#define RT_SIMD_SET16(s, v)     RT_SIMD_SET16_128(s, v)
+#define RT_SIMD_WIDTH08         RT_SIMD_WIDTH08_128
+#define RT_SIMD_SET08(s, v)     RT_SIMD_SET08_128(s, v)
 
 #endif /* RT_SIMD: 2048, 1024, 512, 256, 128 */
 
@@ -256,12 +360,25 @@
 #define RT_SIMD_SET64_256(s, v) s[0]=s[1]=s[2]=s[3]=v
 #define RT_SIMD_WIDTH32_256     8
 #define RT_SIMD_SET32_256(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=v
+#define RT_SIMD_WIDTH16_256     16
+#define RT_SIMD_SET16_256(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
+                                s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=v
+#define RT_SIMD_WIDTH08_256     32
+#define RT_SIMD_SET08_256(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
+                                s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=\
+                              s[16]=s[17]=s[18]=s[19]=s[20]=s[21]=s[22]=s[23]=\
+                              s[24]=s[25]=s[26]=s[27]=s[28]=s[29]=s[30]=s[31]=v
 
 #define RT_SIMD_ALIGN_128       16
 #define RT_SIMD_WIDTH64_128     2
 #define RT_SIMD_SET64_128(s, v) s[0]=s[1]=v
 #define RT_SIMD_WIDTH32_128     4
 #define RT_SIMD_SET32_128(s, v) s[0]=s[1]=s[2]=s[3]=v
+#define RT_SIMD_WIDTH16_128     8
+#define RT_SIMD_SET16_128(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=v
+#define RT_SIMD_WIDTH08_128     16
+#define RT_SIMD_SET08_128(s, v) s[0]=s[1]=s[2]=s[3]=s[4]=s[5]=s[6]=s[7]=\
+                                s[8]=s[9]=s[10]=s[11]=s[12]=s[13]=s[14]=s[15]=v
 
 /*
  * SIMD total-quads (number of 128-bit chunks) for chosen SIMD target.
@@ -273,14 +390,15 @@
 
 /*
  * SIMD width (number of scalar elements) for given SIMD element size.
- * Short names R, S, T represent maximal width for given build config.
+ * Short names J, N, R, S, T represent maximal width for given config.
  * RT_SIMD_WIDTH and S may differ for builds with runtime SIMD target
  * selection in backend's ASM code sections, S is used in SIMD structs.
  */
-#define N   (Q*8)       /* for cmdm*_** SIMD-subset, rt_fp16 SIMD-fields */
-#define R   (Q*4)       /* for cmdo*_** SIMD-subset, rt_fp32 SIMD-fields */
-#define S   (Q*4/L)     /* for cmdp*_** SIMD-subset, rt_real SIMD-fields */
-#define T   (Q*2)       /* for cmdq*_** SIMD-subset, rt_fp64 SIMD-fields */
+#define J   (Q*16)    /* for cmdm*_** SIMD-subset, rt_si08/ui08 SIMD-fields */
+#define N   (Q*8)     /* for cmdm*_** SIMD-subset, rt_si16/ui16 SIMD-fields */
+#define R   (Q*4)     /* for cmdo*_** SIMD-subset, rt_fp32/*i32 SIMD-fields */
+#define S   (Q*4/L)   /* for cmdp*_** SIMD-subset, rt_real/elem SIMD-fields */
+#define T   (Q*2)     /* for cmdq*_** SIMD-subset, rt_fp64/*i64 SIMD-fields */
 
 /*
  * Short names for pointer, address and SIMD element sizes (in 32-bit chunks).
