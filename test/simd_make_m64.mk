740efabc661b2959b0a98896e31380cda80a08b3
VectorChief <vectorchiefrocks@gmail.com>
2026-03-18 22:15:13 +0300

    use upstream MIPS compiler

--- test/simd_make_m64.mk
+++ test/simd_make_m64.mk
@@ -14,8 +14,8 @@ LIB_LIST =                              \
 build: build_le build_be
 
 strip:
-	mips-mti-linux-gnu-strip simd_test.m64???Lr6
-	mips-mti-linux-gnu-strip simd_test.m64???Br6
+	mipsisa64r6el-linux-gnuabi64-strip simd_test.m64???Lr6
+	mipsisa64r6-linux-gnuabi64-strip simd_test.m64???Br6
 
 clean:
 	rm simd_test.m64*
@@ -25,25 +25,25 @@ build_le: simd_test_m64_32Lr6 simd_test_m64_64Lr6 \
           simd_test_m64f32Lr6 simd_test_m64f64Lr6
 
 simd_test_m64_32Lr6:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
+	mipsisa64r6el-linux-gnuabi64-g++ -O3 -g -static -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m64_32Lr6
 
 simd_test_m64_64Lr6:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
+	mipsisa64r6el-linux-gnuabi64-g++ -O3 -g -static -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=64 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m64_64Lr6
 
 simd_test_m64f32Lr6:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
+	mipsisa64r6el-linux-gnuabi64-g++ -O3 -g -static -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_256=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=32 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m64f32Lr6
 
 simd_test_m64f64Lr6:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EL -mips64r6 -mmsa -mabi=64 \
+	mipsisa64r6el-linux-gnuabi64-g++ -O3 -g -static -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_256=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=64 -DRT_ENDIAN=0 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m64f64Lr6
@@ -53,25 +53,25 @@ build_be: simd_test_m64_32Br6 simd_test_m64_64Br6 \
           simd_test_m64f32Br6 simd_test_m64f64Br6
 
 simd_test_m64_32Br6:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
+	mipsisa64r6-linux-gnuabi64-g++ -O3 -g -static -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=32 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m64_32Br6
 
 simd_test_m64_64Br6:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
+	mipsisa64r6-linux-gnuabi64-g++ -O3 -g -static -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_128=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=32 -DRT_ELEMENT=64 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m64_64Br6
 
 simd_test_m64f32Br6:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
+	mipsisa64r6-linux-gnuabi64-g++ -O3 -g -static -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_256=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=32 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m64f32Br6
 
 simd_test_m64f64Br6:
-	mips-mti-linux-gnu-g++ -O3 -g -static -EB -mips64r6 -mmsa -mabi=64 \
+	mipsisa64r6-linux-gnuabi64-g++ -O3 -g -static -mips64r6 -mmsa -mabi=64 \
         -DRT_LINUX -DRT_M64=6 -DRT_256=1 -DRT_DEBUG=0 \
         -DRT_POINTER=64 -DRT_ADDRESS=64 -DRT_ELEMENT=64 -DRT_ENDIAN=1 \
         ${INC_PATH} ${SRC_LIST} ${LIB_PATH} ${LIB_LIST} -o simd_test.m64f64Br6
@@ -83,26 +83,25 @@ simd_test_m64f64Br6:
 # (Ubuntu MATE is set up for an update without a need to edit the file)
 # (extended repositories "universe multiverse" are only needed for clang)
 #
-# Download and unpack MIPS toolchain:
-# https://codescape.mips.com/components/toolchain/2020.06-01/downloads.html
-#
 # Prerequisites for the build:
 # (cross-)compiler for MIPSr6+MSA is installed and in the PATH variable.
+# Starting from Ubuntu (MATE) 19.10 upstream (cross-)compiler supports MSA.
+# sudo apt-get install make g++-mipsisa64r6el-linux-gnuabi64
+# sudo apt-get install make g++-mipsisa64r6-linux-gnuabi64
+#
+# Alternatively use standalone toolchain (comes with QEMU 4.1.0):
+# https://codescape.mips.com/components/toolchain/2020.06-01/downloads.html
 # Codescape.GNU.Tools.Package.2020.06-01.for.MIPS.MTI.Linux.CentOS-6.x86_64
 # is unpacked and folder mips-mti-linux-gnu/2020.06-01/bin is added to PATH:
 # PATH=/home/ubuntu/Downloads/mips-mti-linux-gnu/2020.06-01/bin:$PATH
 # PATH=/home/ubuntu-mate/Downloads/mips-mti-linux-gnu/2020.06-01/bin:$PATH
-#
-# Starting from Ubuntu (MATE) 19.10 upstream (cross-)compiler supports MSA.
-# sudo apt-get install make g++-mipsisa64r6el-linux-gnuabi64
-# sudo apt-get install make g++-mipsisa64r6-linux-gnuabi64
-# (replace mips-mti-linux-gnu with mipsisa64r6el-linux-gnuabi64 for LE)
-# (replace mips-mti-linux-gnu with mipsisa64r6-linux-gnuabi64 for BE)
+# (replace mipsisa64r6el-linux-gnuabi64 with mips-mti-linux-gnu, ...-g++ -EL)
+# (replace mipsisa64r6-linux-gnuabi64 with mips-mti-linux-gnu, ...-g++ -EB)
 #
 # Prerequisites for emulation:
 # recent QEMU(-2.7) is installed or built from source and in the PATH variable.
 # standalone toolchain from 2020.06-01 comes with QEMU 4.1.0 for MIPS in PATH.
-# sudo apt-get install qemu-user make
+# sudo apt-get install qemu-user
 #
 # Compiling/running SIMD test:
 # make -f simd_make_m64.mk
